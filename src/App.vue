1470e7fd843d27599c470527e9bce66eb36b05ae
Noe Cortez <corteznoe3@gmail.com>
2024-06-06 15:47:08 -0600

    Agrega titulo de ejemplo

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 	<header>
 		<img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
+		<h1>Vite + Vue</h1>
 		<nav>
 			<RouterLink to="/">Home</RouterLink>
 			<RouterLink to="/about">About</RouterLink>
